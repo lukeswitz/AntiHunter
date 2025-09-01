3718df614c75c2154e78970dbb7f647485d79b31
Luke Switzer <10099969+lukeswitz@users.noreply.github.com>
2025-09-01 09:09:36 -0600

    Pin defs for prototype
    -  Drops support for mesh-detect board: Change mesh rx pin from 4 > 6 if using the colpanic board

--- Antihunter_Mesh/src/hardware.h
+++ Antihunter_Mesh/src/hardware.h
@@ -13,21 +13,21 @@
 #define BUZZER_IS_PASSIVE 1
 #endif
 #ifndef MESH_RX_PIN
-#define MESH_RX_PIN 6   // to MESH TX PIN 20
+#define MESH_RX_PIN 4   // to MESH PIN 20   (GPIO4) - CHANGE TO MESH_RX_PIN 6 FOR MESH-DETECT BOARD
 #endif
 #ifndef MESH_TX_PIN
-#define MESH_TX_PIN 5   // to MESH RX PIN 19
+#define MESH_TX_PIN 5   // to MESH PIN 19   (GPIO5)
 #endif
 
 // SD Card (SPI)
-#define SD_CS_PIN   2    // CS on D1 (GPIO2)
+#define SD_CS_PIN   2    // CS on D1        (GPIO2)
 #define SD_CLK_PIN  7    // CLK (SCK) on D8 (GPIO7)
-#define SD_MISO_PIN 8    // MISO on D9 (GPIO8)
-#define SD_MOSI_PIN 9    // MOSI on D10 (GPIO9)
+#define SD_MISO_PIN 8    // MISO on D9      (GPIO8)
+#define SD_MOSI_PIN 9    // MOSI on D10     (GPIO9)
 
 // GPS (UART)
-#define GPS_RX_PIN 44   // D7 = GPIO44 (ESP RX)
-#define GPS_TX_PIN 43   // D6 = GPIO43 (ESP TX)
+#define GPS_RX_PIN 44   // D7 = GPIO44      (ESP RX)
+#define GPS_TX_PIN 43   // D6 = GPIO43      (ESP TX)
 
 extern bool sdAvailable;
 extern bool gpsValid;
