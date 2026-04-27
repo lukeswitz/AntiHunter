4f140b11e834301127ed30158b882f32402f77d3
Luke Switzer <10099969+lukeswitz@users.noreply.github.com>
2026-04-27 09:32:17 -0600

    bump build

--- Antihunter/full/src/network.cpp
+++ Antihunter/full/src/network.cpp
@@ -1036,7 +1036,7 @@ static const char INDEX_HTML[] PROGMEM = R"HTML(
 
       </div>
 
-      <div class="footer">AntiHunter DIGINODE v0.9.3 | Node: <span id="footerNodeId">--</span></div>
+      <div class="footer">AntiHunter DIGINODE v0.9.4 | Node: <span id="footerNodeId">--</span></div>
     
       <script>
       let tickRunning = false;
